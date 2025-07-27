60bba40eee9768fbd07cefccb771c3147e2d690b
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-07-27 20:34:45 +0200

    Added an image loader

--- GLFW_Test/src/Game/MyGameApp.cpp
+++ GLFW_Test/src/Game/MyGameApp.cpp
@@ -23,6 +23,12 @@ void MyGameApp::OnStart() {
 	unsigned int indices[] = {
 		0, 1, 2
 	};
+	float texCoords[] = {
+		0.0f, 0.0f, // lower-left corner
+		1.0f, 0.0f, // lower-right corner
+		0.5f, 1.0f // top-center corner
+	};
+
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
