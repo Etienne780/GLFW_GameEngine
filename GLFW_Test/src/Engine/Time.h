f37831b62e1c9fd0b88d207c07c47851354ca3f6
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-07-30 01:09:33 +0200

    Add comments

--- GLFW_Test/src/Engine/Time.h
+++ GLFW_Test/src/Engine/Time.h
@@ -1,15 +1,20 @@
 #pragma once
 
 namespace EngineCore {
-	class Time {
-	public:
-		static void UpdateTime(double currentTimeSec);
+	class Engine;
 
+	class Time {
+		friend class Engine;
+
+	public:
+		// Gets the time since the programm started
 		static double GetTimeSec();
+		// Gets the time diff between current frame and last frame time
 		static double GetDeltaTimeSec();
 
 	private:
 		Time();
+		static void UpdateTime(double currentTimeSec);
 		static void CalculateDeltaTime(double currentTimeSec);
 
 		static double timeSec;
