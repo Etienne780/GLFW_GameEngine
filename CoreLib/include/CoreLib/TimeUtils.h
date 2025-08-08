8b886d878ea083ca90818e8ae6ac9f122262369e
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-08-08 17:53:46 +0200

    Get Date function

---
+++ CoreLib/include/CoreLib/TimeUtils.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+class TimeUtils {
+public:
+    static std::string GetCurrentDateString();
+
+private:
+    TimeUtils();
+};
+
---
+++ CoreLib/src/CoreLib/TimeUtils.cpp
@@ -0,0 +1,17 @@
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+#include "CoreLib\TimeUtils.h"
+
+std::string TimeUtils::GetCurrentDateString() {
+    auto t = std::time(nullptr);
+    std::tm tm{};
+#ifdef _WIN32
+    localtime_s(&tm, &t);
+#else
+    localtime_r(&t, &tm);
+#endif
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%Y-%m-%d");
+    return oss.str();
+}
