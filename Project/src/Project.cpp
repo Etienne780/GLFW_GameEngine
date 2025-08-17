fda60af6bdef7d59e36f111848e697806bc0204b
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-08-17 19:22:52 +0200

    rotating sphere

--- Project/src/Project.cpp
+++ Project/src/Project.cpp
@@ -1,4 +1,4 @@
-#include "Project.h"
+﻿#include "Project.h"
 
 using namespace EngineCore;
 
@@ -20,10 +20,14 @@ Project::Project()
 	// Log::SaveLogs("Logs/");
 }
 
+void GenerateCubesSphere();
+
 std::shared_ptr<GameObject> cameraGO = nullptr;
-std::vector<std::shared_ptr<GameObject>> cubes;
-size_t cubeCountTheta = 20; // horizontale Segmente
-size_t cubeCountPhi = 20;   // vertikale Segmente
+std::shared_ptr<Component::Transform> containerTrans = nullptr;
+
+size_t cubeCountTheta = 50; // horizontale Segmente
+size_t cubeCountPhi = 50;   // vertikale Segmente
+float sphereRadius = 100.0f;
 void Project::Start() {
 	App_OpenGL_Set_BackgroundColor(0.2f, 0.3f, 0.3f);
 	// App_OpenGL_Set_PolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -33,12 +37,17 @@ void Project::Start() {
 	auto cam = cameraGO->AddComponent<Component::Camera>();
 	cameraGO->AddComponent<Component::FreeCameraController>();
 
-	auto c = GameObject::Create("TestCube");
-	auto mr = c->AddComponent<Component::MeshRenderer>();
-	mr->SetMesh(ID::MESH::ENGINE::Cube()).SetMaterial(ID::MATERIAL::ENGINE::Default());
-	c->GetTransform()->SetScale(10, 10, 10);
+	// auto c = GameObject::Create("TestCube");
+	// auto mr = c->AddComponent<Component::MeshRenderer>();
+	// mr->SetMesh(ID::MESH::ENGINE::Cube()).SetMaterial(ID::MATERIAL::ENGINE::Default());
+	// c->GetTransform()->SetScale(10, 10, 10);
 
-	float radius = 100.0f;
+	GenerateCubesSphere();
+}
+
+void GenerateCubesSphere() {
+	auto container = GameObject::Create("Container");
+	containerTrans = container->GetTransform();
 
 	for (size_t i = 0; i < cubeCountTheta; ++i) {
 		float theta = static_cast<float>((static_cast<float>(i) / cubeCountTheta) * 2.0f * CORE_PI);
@@ -46,39 +55,23 @@ void Project::Start() {
 		for (size_t j = 0; j < cubeCountPhi; ++j) {
 			float phi = static_cast<float>((static_cast<float>(j) / cubeCountPhi) * CORE_PI);
 
-			float x = radius * sin(phi) * cos(theta);
-			float y = radius * cos(phi);
-			float z = radius * sin(phi) * sin(theta);
+			float x = sphereRadius * sin(phi) * cos(theta);
+			float y = sphereRadius * cos(phi);
+			float z = sphereRadius * sin(phi) * sin(theta);
 
 			auto cubeGO = GameObject::Create(FormatUtils::formatString("Cube_{}_{}", i, j));
+			cubeGO->SetParent(container);
 			auto mr = cubeGO->AddComponent<Component::MeshRenderer>();
 			mr->SetMesh(ID::MESH::ENGINE::Cube()).SetMaterial(ID::MATERIAL::ENGINE::Default());
 
 			cubeGO->GetTransform()->SetPosition(x, y, z);
 			cubeGO->GetTransform()->SetScale(5, 5, 5);
-
-			cubes.push_back(std::move(cubeGO));
 		}
 	}
 }
 
-void UpdateCubes(std::vector<std::shared_ptr<GameObject>>& cubes, float time, size_t thetaCount, size_t phiCount) {
-	float radius = 100.0f;
-	size_t index = 0;
-
-	for (size_t i = 0; i < thetaCount; ++i) {
-		float theta = static_cast<float>((static_cast<float>(i) / thetaCount) * 2.0f * CORE_PI + time);
-
-		for (size_t j = 0; j < phiCount; ++j) {
-			float phi = static_cast<float>((static_cast<float>(j) / phiCount) * CORE_PI);
-
-			float x = radius * sin(phi) * cos(theta);
-			float y = radius * cos(phi);
-			float z = radius * sin(phi) * sin(theta);
-
-			cubes[index++]->GetTransform()->SetPosition(x, y, z);
-		}
-	}
+void UpdateCubesSphere(float time) {
+	containerTrans->SetRotation(time, time * 1.235f, 0);
 }
 
 void Project::Update() {
@@ -93,9 +86,7 @@ void Project::Update() {
 		App_Application_Set_Window_Floating(!App_Application_Get_Window_Floating());
 	}
 
-	UpdateCubes(cubes, Time::GetTime(), cubeCountTheta, cubeCountPhi);
-
-	Log::Info("FPS: {}", App_Application_Get_FramesPerSecond());
+	UpdateCubesSphere(Time::GetTime());
 }
 
 void Project::Shutdown() {
