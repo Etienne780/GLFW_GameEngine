6084fdaeaac1f81684380cda5680336be2fc5d3d
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-08-29 20:10:22 +0200

    changed include slash

--- EngineLib/include/EngineLib/Engine.h
+++ EngineLib/include/EngineLib/Engine.h
@@ -4,28 +4,29 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
-#include <CoreLib\Log.h>
-#include <CoreLib\Math.h>
-#include "CoreLib\File.h"
-#include <CoreLib\FormatUtils.h>
-#include <CoreLib\ColorUtils.h>
-#include <CoreLib\TimeUtils.h>
-#include <CoreLib\ConversionUtils.h>
-#include <CoreLib\stb_image.h>
+#include <CoreLib/Log.h>
+#include <CoreLib/Math.h>
+#include "CoreLib/File.h"
+#include <CoreLib/FormatUtils.h>
+#include <CoreLib/ColorUtils.h>
+#include <CoreLib/TimeUtils.h>
+#include <CoreLib/ConversionUtils.h>
+#include <CoreLib/stb_image.h>
 
-#include "EngineLib\EngineTypes.h"
-#include "EngineLib\Application.h"
-#include "EngineLib\Time.h"
-#include "EngineLib\Input.h"
-#include "EngineLib\Shader.h"
-#include "EngineLib\Texture2D.h"
-#include "EngineLib\Material.h"
-#include "EngineLib\ResourceManager.h"
-#include "EngineLib\AssetRepository.h"
-#include "EngineLib\GameObjectManager.h"
-#include "EngineLib\GameObject.h"
-#include "EngineLib\AllComponents.h"
-#include "EngineLib\RenderLayer.h"
+#include "EngineLib/EngineTypes.h"
+#include "EngineLib/Application.h"
+#include "EngineLib/Time.h"
+#include "EngineLib/Input.h"
+#include "EngineLib/Shader.h"
+#include "EngineLib/Texture2D.h"
+#include "EngineLib/Material.h"
+#include "EngineLib/ResourceManager.h"
+#include "EngineLib/AssetRepository.h"
+#include "EngineLib/GameObjectManager.h"
+#include "EngineLib/GameObject.h"
+#include "EngineLib/AllComponents.h"
+#include "EngineLib/RenderLayer.h"
+#include "EngineLib/PrefabManager.h"
 
 namespace EngineCore {
 
