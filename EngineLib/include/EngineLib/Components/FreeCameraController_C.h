93fe9d38a157c355abcc3489e44ef8e467dfa015
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-08-31 13:11:25 +0200

    chan: changed default values

--- EngineLib/include/EngineLib/Components/FreeCameraController_C.h
+++ EngineLib/include/EngineLib/Components/FreeCameraController_C.h
@@ -36,7 +36,7 @@ namespace EngineCore {
 			float m_arrowSensitivity = 0.8f;
 			float m_mouseSensitivity = 0.2f;
 
-			float m_movementSpeed = 30.0f;
+			float m_movementSpeed = 10.0f;
 			float m_sprintMultiplier = 2.5f;
 			float m_slowMultiplier = 0.4f;
 			float m_verticalMovementspeedMultiplier = 1.2f;
